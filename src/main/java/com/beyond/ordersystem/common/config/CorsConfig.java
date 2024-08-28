fd7575ad5f1172f73034880396db32a1d30286dd
snikuz12 <snikuz12@naver.com>
2024-08-28 10:39:26 +0900

    fix

--- src/main/java/com/beyond/ordersystem/common/config/CorsConfig.java
+++ src/main/java/com/beyond/ordersystem/common/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://www.teenkiri.store") //허용 url 명시
+                .allowedOrigins("http://www.teenkiri.store")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true); //보안처리
